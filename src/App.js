c8e3d0d68b909174f790173f0066b44be70f0e26
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-09-22 16:05:29 -0300

    Add login component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom'
+import Login from './login/index';
 
 class App extends Component {
+
+  login() {
+    ReactDOM.render(
+      <Login />,
+      document.getElementById('center')
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +20,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="center">
+          <p className="App-intro">
+            <a href="#" onClick={() => {this.login()}}>LOGIN</a>
+          </p>
+        </div>
       </div>
     );
   }
