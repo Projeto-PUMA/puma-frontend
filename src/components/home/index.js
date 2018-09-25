45607a34049146590d8cd51e7ae9d1e0e0f3cd54
thiagoiferreira <thiagoiferreira@outlook.com>
2018-09-25 18:06:37 -0300

    Initial Structure for Home Page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,76 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Carousel from '../carousel/index.js'
+import CarouselApp from '../carousel/index.js';
+import { Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle, Row, Col } from 'reactstrap';
 
 class Home extends Component {
   render() {
     return (
-      <div id="center" style={{ marginTop: 30, marginBottom: 30 }}>
-        <h1>BEM VINDO AO PUMA</h1>
+      <div id="center" style={{ marginTop: 30, marginBottom: 30, marginLeft:40, marginRight:40, align:'center' }}>
+        <div >
+      <Row >
+      <Col sm="1"/>
+        <Col sm="10">
+      <Card>
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+        </CardBody>
+        <CarouselApp />        
+        <CardBody>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <CardLink href="#">Card Link</CardLink>
+          <CardLink href="#">Another Link</CardLink>
+        </CardBody>
+      </Card>
+      </Col>
+      </Row>
+      <Row>
+        <Col sm="1"/>
+        <Col sm="3">
+          <Card>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+          </CardBody>
+          <CardBody>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+        </Col>
+        <Col sm="4">
+          <Card>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+          </CardBody>
+          <CardBody>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+        </Col>
+        <Col sm="3">
+          <Card>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+          </CardBody>
+          <CardBody>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+        </Col>
+      </Row>
+    </div>
       </div>
+      
     );
   }
 }
