396c190860355d66b69304ae42eaa7db3019343c
Icaro Oliveira <icarooliv2@gmail.com>
2018-09-25 15:56:42 -0300

    adds reactstrap

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-
   render() {
     return (
-      <div id="center">
+      <div id="center" style={{ marginTop: 30, marginBottom: 30 }}>
         <h1>BEM VINDO AO PUMA</h1>
       </div>
     );
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 import * as jwt_decode from "jwt-decode";
-import ReactDOM from 'react-dom';
-import Home from '../home/index';
-import * as Store from '../../store';
+import ReactDOM from "react-dom";
+import Home from "../home/index";
+import * as Store from "../../store";
+import { Button } from "reactstrap";
 
 class Login extends Component {
-  
   constructor(props) {
     super(props);
     this.handleLogin = this.handleLogin.bind(this);
@@ -20,33 +20,39 @@ class Login extends Component {
       data[field] = this.refs[field].value;
     }
 
-    const path = Store['backend'].path; // This is backend path
-    axios.post(path + '/auth', {
-      username: data['username'],
-      password: data['password'],
-    })
-    .then(response => { this.setLogin(response) })
-    .catch(function (error) {
-      if (error) {
-        alert('Usuário não cadastrado!');
-      }
-    });
+    const path = Store["backend"].path; // This is backend path
+    axios
+      .post(path + "/auth", {
+        username: data["username"],
+        password: data["password"]
+      })
+      .then(response => {
+        this.setLogin(response);
+      })
+      .catch(function(error) {
+        if (error) {
+          alert("Usuário não cadastrado!");
+        }
+      });
   }
 
   setLogin(response) {
-    if (response.status===200) {
-      localStorage.setItem('currentUser', JSON.stringify({token: response.data.token }));
-      let tokenInfo = this.getDecodedAccessToken(this.getToken());
-      localStorage.setItem('authorities', JSON.stringify(tokenInfo.authorities));
-      ReactDOM.render(
-        <Home />,
-        document.getElementById('center')
+    if (response.status === 200) {
+      localStorage.setItem(
+        "currentUser",
+        JSON.stringify({ token: response.data.token })
       );
+      let tokenInfo = this.getDecodedAccessToken(this.getToken());
+      localStorage.setItem(
+        "authorities",
+        JSON.stringify(tokenInfo.authorities)
+      );
+      ReactDOM.render(<Home />, document.getElementById("center"));
     }
   }
 
   getToken() {
-    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
     var token = currentUser && currentUser.token;
     return token ? token : null;
   }
@@ -54,64 +60,62 @@ class Login extends Component {
   getDecodedAccessToken(token) {
     try {
       return jwt_decode(token);
-    }
-    catch(Error){
+    } catch (Error) {
       return null;
     }
   }
 
   getUserId() {
-    try{
+    try {
       let token = this.getToken();
       let tokenInfo = jwt_decode(token);
       return tokenInfo.id;
-    }
-    catch(Error){
+    } catch (Error) {
       return null;
     }
   }
 
   isAuthenticated() {
     // get the auth token from localStorage
-    let token = localStorage.getItem('currentUser');
+    let token = localStorage.getItem("currentUser");
     // check if token is set, then...
     if (token) {
-        return true;
+      return true;
     }
     return false;
   }
 
   logout() {
-    try{
-      if(localStorage.getItem('currentUser')){
-        localStorage.removeItem('authorities');
-        localStorage.removeItem('currentUser');
+    try {
+      if (localStorage.getItem("currentUser")) {
+        localStorage.removeItem("authorities");
+        localStorage.removeItem("currentUser");
         return true;
-      }
-      else {
+      } else {
         return false;
       }
-    }
-    catch(Error){
+    } catch (Error) {
       return false;
     }
   }
 
   render() {
     return (
-        <div /*style={{height: "100vh"}}*/>
-          <form onSubmit={this.handleLogin}>
-            <label>
-              CPF:
-              <input ref="username" className="username" type='text' name="username"/>
-            </label>
-            <label>
-              Senha:
-              <input ref="password" className="password" type='password' name="password"/>
-            </label>
-            <input type="submit" value="Submit"/>
-          </form>
-        </div>
+      <div /*style={{height: "100vh"}}*/>
+        <form>
+          <label>
+            CPF:
+            <input ref="username" type="text" name="username" />
+          </label>
+          <label>
+            Senha:
+            <input ref="password" type="password" name="password" />
+          </label>
+        </form>
+        <Button type="submit" color="primary" onClick={this.handleLogin}>
+          Entrar
+        </Button>
+      </div>
     );
   }
 }
