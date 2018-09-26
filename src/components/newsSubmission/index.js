09716c98796ee049b06cfd2e9260214d3fceba84
thiagoiferreira <thiagoiferreira@outlook.com>
2018-09-26 20:37:49 -0300

    Adding styling and adapting submition to project submission

--- src/components/newsSubmission/index.js
+++ src/components/newsSubmission/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import * as jwt_decode from "jwt-decode";
 import * as Store from '../../store';
+import {Col,Row,Input,Label,Button,FormGroup,Card,CardBody,Form} from 'reactstrap';
 
 class NewsSubmission extends Component {
 
@@ -22,10 +23,7 @@ class NewsSubmission extends Component {
 	handleNews(e) {
     e.preventDefault();
 
-    const data = {};
-    for (const field in this.refs) {
-      data[field] = this.refs[field].value;
-    }
+    const data = new FormData(e.target);
 
 		var currentUser = JSON.parse(localStorage.getItem('currentUser'));
     var token = currentUser && currentUser.token;
@@ -36,8 +34,8 @@ class NewsSubmission extends Component {
     const path = Store['backend'].path; // This is backend path
     axios.post(path + '/sec/post/new', {
       author: { id: tokenInfo.id },
-			title: data['title'],
-			body: data['body']
+			title: data.get('title'),
+			body: data.get('body')
     })
     .then(() => { alert('Notícia criada com sucesso!') })
     .catch(function (error) {
@@ -50,17 +48,45 @@ class NewsSubmission extends Component {
   render() {
     return (
 			<div>
-				<form onSubmit={this.handleNews}>
-					<label>
-						Título da Notícia:
-						<input ref="title" className="title" type='text' name="title"/>
-					</label>
-					<label>
-						Descrição:
-						<input ref="body" className="body" type='text' name="body"/>
-					</label>
-					<input type="submit" value="Submit"/>
-				</form>
+        <Row>
+          <Col sm='2' md='3' lg='4' xs='1'/>
+          <Col sm='6' md='5' lg='4' xs='10' style={{textAlign:'center'}}><h2>Submissão de Projeto</h2></Col>
+          </Row>
+        <Row>
+          <Col sm='1' md='2' lg='3' xs='1'/>
+          <Col sm='8' md='7' lg='6' xs='10'>
+          <Card>
+          <CardBody>
+          <Form
+            id='projectSubmissionForm'
+            name='projectSubmissionForm'
+            onSubmit={this.handleNews}
+          >
+            <FormGroup>
+              <Label >Título da Notícia</Label>
+              <Input
+                ref='title'
+                type='text'
+                name='title'
+                id='title'
+                required/>
+            </FormGroup>
+            <FormGroup>
+              <Label>Conteúdo/Corpo</Label>
+              <Input
+                 ref='body'
+                 type='textarea'
+                 name='body'
+                 id='body'/>
+            </FormGroup>
+            <Button type="submit" value ="submit" color="primary" style={{ display: "block",margin: "0 auto"}}>
+              Enviar Projeto
+            </Button>
+          </Form>
+          </CardBody>
+          </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
