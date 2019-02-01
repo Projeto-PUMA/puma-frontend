da24352594541497fff9d1cff32b498670b37eec
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-02-01 14:08:36 -0200

    Arrumando o cadastro

--- src/actions/user/index.js
+++ src/actions/user/index.js
@@ -2,15 +2,18 @@
 import userApi from '../../api/user';
 // import * as UserAC from './actionCreators';
 import * as SyncOperationAC from '../meta/actionCreators';
+import { browserHistory } from 'react-router';
 
 export const createUser = (name, email, password, cpf, scholarity, cep, state, city, neighborhood, street, number, addendum, address_category_id, occupation_id, phone) => (dispatch) => {
   const thenCallback = (result) => {
     dispatch(SyncOperationAC.syncOperationFinished(result));
+    console.log(    dispatch(SyncOperationAC.syncOperationFinished(result))    )
     // dispatch(loadNews());
     // browserHistory.push('/gerenciarnoticias');
-    // if (result.status === 200) {
-    //   alert('Notícia criada com sucesso!');
-    // }
+    if (result.status === 200) {
+      alert('Usuário cadastrado com sucesso!');
+      browserHistory.push('/login');
+    }
   };
 
   const catchCallback = (error) => {
