a89491ed371e5d5114d1721be94f8fa9495aa1c9
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-02-17 12:14:15 -0300

    Adding new colors

--- src/components/sidebar/materialTitlePanel.js
+++ src/components/sidebar/materialTitlePanel.js
@@ -8,7 +8,7 @@ const styles = {
     fontWeight: 300
   },
   header: {
-    backgroundColor: "#03a9f4",
+    backgroundColor: "#09545e",
     color: "white",
     padding: "16px",
     fontSize: "1.5em"
